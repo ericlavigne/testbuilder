90dea0829fe4d58f49c9ce8317d35b3a17f56424
Eric Lavigne <lavigne.eric@gmail.com>
2010-09-29 20:16:00 -0400

    Created textile-to-html-fragment function that uses textile-j.

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject testbuilder "0.0.1"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [net.java/textile-j "2.2"]]
+  :repositories {"java.net" "http://download.java.net/maven/2"})
--- src/testbuilder/core.clj
+++ src/testbuilder/core.clj
@@ -1,1 +1,15 @@
-(ns testbuilder.core)
+(ns testbuilder.core
+  (:import (net.java.textilej.parser.builder HtmlDocumentBuilder))
+  (:import (net.java.textilej.parser MarkupParser))
+  (:import (net.java.textilej.parser.markup.textile TextileDialect)))
+
+; http://www.textism.com/tools/textile/index.php
+(defn textile-to-html-fragment [textile]
+  (let [out (java.io.StringWriter.)
+	builder (HtmlDocumentBuilder. out)
+	parser (doto (MarkupParser.)
+		 (.setDialect (TextileDialect.))
+		 (.setBuilder builder)
+		 (.parse textile false))]
+    (.toString out)))
+
