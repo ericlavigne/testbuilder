22485f449c13681d5e07c6dab5b228c40545101a
Algarete13 <joelmontero13@gmail.com>
2010-09-29 20:36:31 -0400

    Wrote test for textile converter

--- test/testbuilder/test/core.clj
+++ test/testbuilder/test/core.clj
@@ -2,5 +2,10 @@
   (:use [testbuilder.core] :reload)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest textile-to-html-fragment-test
+  (is (= "<p>m/s <sup>2</sup></p>"
+     (textile-to-html-fragment "m/s ^2^"))
+     "Testing Superscript")
+  (is (= "<p>V <sub>o</sub></p>"
+      (textile-to-html-fragment "V ~o~"))
+      "Testing subscrpit"))
